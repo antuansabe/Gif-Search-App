a00a8de11353ff6da6df448e988dcbf44f746d7a
Antonio Dromundo <72485462+antuansabe@users.noreply.github.com>
2021-07-28 00:55:59 -0500

    Update GifGrid.js

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { GifGridItem } from './GifGridItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
